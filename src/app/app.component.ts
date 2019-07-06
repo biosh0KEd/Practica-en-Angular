b1c07064e11393be0c8c5c1e11c44140c512ec31
Efrain <efrainbond@hotmail.com>
2019-07-06 12:45:08 -0500

    Creado un ejemplo de animar un cuadrado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import {trigger, state, style, transition, animate} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('contenedorAnimable', [
+      state('inicial', style({
+        opacity: 0,
+        backgroundColor: 'green',
+        transform: 'rotate3d(0,0,0,0deg)'
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor: 'yellow',
+        transform: 'rotate3d(5,10,20,30deg)'
+      })),
+      transition('inicial => final', animate(1000)),
+      transition('final => inicial', animate(500))
+    ])
+  ]
 })
 export class AppComponent {
   title = 'platzi-test';
+  state = 'inicial';
+  animar(){
+    this.state = (this.state === 'final') ? 'inicial' : 'final';
+  }
 }
